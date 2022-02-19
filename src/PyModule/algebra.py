1d84d4b7b0475fdae36f9087c8dd28470248bcc5
cisimon7 <cisimon7@gmail.com>
2022-02-19 11:24:37 +0300

    Pybind requirement

--- src/PyModule/algebra.py
+++ src/PyModule/algebra.py
@@ -1,8 +1,8 @@
-import bindings.Algebra as Alg
+from bindings.Algebra import cross
 import numpy as np
 
 if __name__ == '__main__':
     print("Hello World")
 
-    result = Alg.cross([3, 3, 3], [1, 2, 1])
+    result = cross([3, 3, 3], [1, 2, 1])
     print(result)
